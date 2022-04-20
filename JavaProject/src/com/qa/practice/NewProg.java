30d49417675be5f6ddc55adacb3dfdb9b7367ef3
hanmanthudekar <hanmanthudekar@gmail.com>
2022-04-20 21:58:25 +0530

    commit

--- JavaProject/src/com/qa/practice/NewProg.java
+++ JavaProject/src/com/qa/practice/NewProg.java
@@ -2,7 +2,8 @@ package com.qa.practice;
 
 public class NewProg {
 
-	public static void main() {
+	public static void main(String [] args) {
 		System.out.println("This is feature branch");
+		System.out.print("Hello");
 	}
 }
